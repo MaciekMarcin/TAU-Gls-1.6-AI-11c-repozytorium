3e9831de78cdd41578c15b6b455df51c21815f1c
MaciekMarcin <32327154+MaciekMarcin@users.noreply.github.com>
2019-03-12 11:15:42 +0100

    Add files via upload
    Niedokoczone lab 2

---
+++ labTwoApp/src/test/java/pl/s15778/tau/candle/DatabaseTestSuite.java
@@ -0,0 +1,12 @@
+package pl.s15778.tau.candle.domain;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import pl.tau.dbdemo.dao.PersonDaoTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(PersonDaoTest.class)
+public class DatabaseTestSuite
+{
+
+}
